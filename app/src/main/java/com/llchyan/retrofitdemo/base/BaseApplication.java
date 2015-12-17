5534187e74a4e9d1af07ee9250fa60bf6d137567
llchyan <haku_kuon@sina.com>
2015-12-17 09:09:57 +0800

    建立工程

---
+++ app/src/main/java/com/llchyan/retrofitdemo/base/BaseApplication.java
@@ -0,0 +1,16 @@
+package com.llchyan.retrofitdemo.base;
+
+import android.app.Application;
+
+/**
+ * Created by LinLin on 2015/12/5.
+ */
+public class BaseApplication extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+//        Fresco.initialize(this);
+    }
+}
